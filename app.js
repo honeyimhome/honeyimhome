49108fa9761499de4ff1a41d1ddf74344224d287
Sergio Puleri <spuleri@ufl.edu>
2016-01-23 15:24:52 -0500

    endpoint for creating household done

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(lusca({
-  csrf: true,
+  csrf: false,
   xframe: 'SAMEORIGIN',
   xssProtection: true
 }));
--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,36 @@
 $(document).ready(function() {
 
-    // Place JavaScript code here...
+  // Place JavaScript code here...
 
-    // ### SIDE-NAV SHIT HERE.
-    // Initialize collapse button
-    $(".button-collapse").sideNav();
-    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    //$('.collapsible').collapsible();
+  // ### SIDE-NAV SHIT HERE.
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
 
-    // Show sideNav
-    $('.button-collapse').sideNav('show');
-    // Hide sideNav
-    $('.button-collapse').sideNav('hide');
+  // Show sideNav
+  $('.button-collapse').sideNav('show');
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
 
-    // ### Button to create household
-    $( "#create-household" ).click(function() {
-        // alert( "Handler for .click() called." );
-        createHousehold();
-    });
+  // ### Button to create household
+  $("#create-household").click(function() {
+    // alert( "Handler for .click() called." );
+    createHousehold();
+  });
 
 
-    createHousehold = function() {
-        $.ajax({
-          method: "POST",
-          url: "http://localhost:3000/household",
-          data: { name: "John", location: "Boston" }
-        })
-          .done(function( msg ) {
-            alert( "Data Saved: " + msg );
-          });
-    }
+  createHousehold = function() {
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:3000/household",
+      data: {
+        name: "John"
+      }
+    })
+      .done(function(msg) {
+        alert("Data Saved: " + msg);
+      });
+  };
 
 });
