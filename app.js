efbdb3ff304f70a709abcadd44b65d426209b90b
Sergio Puleri <spuleri@ufl.edu>
2016-01-23 21:00:26 -0500

    create album on brads end when creating a household in web app

--- app.js
+++ app.js
@@ -160,23 +160,24 @@ app.route('/household')
 //.delete(householdController.deleteBooks)
 
 // Send in an image to be recognized.
-app.post('/api/recognizeImage', function(req, res){
- // Find a unique file name to store the image temporarily
- var imageName = new Date().getTime() + ".jpg";
- var imagePath = "./image_processing/images/" + imageName;
- // Convert the image from base64
- fs.writeFile(imagePath, new Buffer(req.body.image, "base64"), function(err) {
-   lambdaClient.recognizeFace(req.body.albumName, req.body.albumKey, imagePath, function(result, error){
-     if(error) throw error;
-     var person = result.photos[0].tags[0].uids[0];
-     if(person.confidence < 0.5){
-       res.send("guest");
-     }else{
-       res.send(person.prediction);
-     }
-     fs.unlink(imagePath);
-   });
- });
+app.post('/api/recognizeImage', function(req, res) {
+  // Find a unique file name to store the image temporarily
+  var imageName = new Date().getTime() + ".jpg";
+  var imagePath = "./image_processing/images/" + imageName;
+  // Convert the image from base64
+  fs.writeFile(imagePath, new Buffer(req.body.image, "base64"), function(err) {
+    lambdaClient.recognizeFace(req.body.albumName, req.body.albumKey, imagePath, function(result, error) {
+      if (error)
+        throw error;
+      var person = result.photos[0].tags[0].uids[0];
+      if (person.confidence < 0.5) {
+        res.send("guest");
+      } else {
+        res.send(person.prediction);
+      }
+      fs.unlink(imagePath);
+    });
+  });
 });
 
 /***
