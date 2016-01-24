9db7833ded755ac2b3f8dd743caf5a78cd7606dc
acolas1 <acolas1@ufl.edu>
2016-01-24 00:12:20 -0500

    working on client

--- app.js
+++ app.js
@@ -42,9 +42,11 @@ dotenv.load({
 var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
+var clientController = require('./controllers/client');
 var contactController = require('./controllers/contact');
 var householdController = require('./controllers/household');
 var lambdaClient = require('./image_processing/lambdaClient');
+var smsController = require('./controllers/text');
 
 
 /**
@@ -158,30 +160,7 @@ app.post('/household', upload.array('selfies', 4), householdController.joinHouse
 //.delete(householdController.deleteBooks)
 
 // Send in an image to be recognized.
-app.post('/api/recognizeImage', function(req, res){
- // Find a unique file name to store the image temporarily
- var imageName = new Date().getTime() + ".jpg";
- var imagePath = "./image_processing/images/" + imageName;
- // Convert the image from base64
- fs.writeFile(imagePath, new Buffer(req.body.image, "base64"), function(err) {
-   lambdaClient.recognizeFace(req.body.albumName, req.body.albumKey, imagePath, function(result, error){
-     console.log(result);
-     if(error) throw error;
-     if(result.photos[0].tags.length == 0){
-       res.send('###');
-     }else{
-       var person = result.photos[0].tags[0].uids[0];
-       if(person.confidence < 0.5){
-         res.send("guest");
-       }else{
-         res.send(person.prediction);
-       }
-     }
-
-     fs.unlink(imagePath);
-   });
- });
-});
+app.post('/api/recognizeImage', client.picRecognize);
 
 /***
 *OUR APP VIEWS
