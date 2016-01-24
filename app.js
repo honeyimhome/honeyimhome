840bcf56d99bb925c472cd4ef465a4760e9c9c53
nicolasiscoding <nicolascoding@gmail.com>
2016-01-23 22:33:42 -0500

    bug squashin

--- app.js
+++ app.js
@@ -167,13 +167,19 @@ app.post('/api/recognizeImage', function(req, res){
  // Convert the image from base64
  fs.writeFile(imagePath, new Buffer(req.body.image, "base64"), function(err) {
    lambdaClient.recognizeFace(req.body.albumName, req.body.albumKey, imagePath, function(result, error){
+     console.log(result);
      if(error) throw error;
-     var person = result.photos[0].tags[0].uids[0];
-     if(person.confidence < 0.5){
-       res.send("guest");
+     if(result.photos[0].tags.length == 0){
+       res.send('###');
      }else{
-       res.send(person.prediction);
+       var person = result.photos[0].tags[0].uids[0];
+       if(person.confidence < 0.5){
+         res.send("guest");
+       }else{
+         res.send(person.prediction);
+       }
      }
+
      fs.unlink(imagePath);
    });
  });
